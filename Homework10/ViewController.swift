d993f476e2f0fbedf59f60a092aea683493ca111
TheMrArthur <arturgorlov5@gmail.com>
2022-07-28 21:34:48 +0300

    Initial Commit

---
+++ Homework10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework10
+//
+//  Created by Артур Горлов on 28.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
